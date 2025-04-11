2f328824c2b5db557686412bd7775920f596694c
Mohsen Shamsitabar <shamsitabar.mohsen@gmail.com>
2025-04-11 13:50:15 +0330

    refactor

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,7 @@
+// === === === WWEB === === === //
+
+export type Command = "help" | "ping" | "login";
+
 export type User = {
   // works like ID.
   phoneNumber: string;
--- src/whatsapp-web/handlers/messages.ts
+++ src/whatsapp-web/handlers/messages.ts
@@ -1,30 +1,31 @@
 import type WAWebJS from "whatsapp-web.js";
-import { CMD_CHAR } from "../config.ts";
+import { Command } from "../../types.ts";
+import { CMD_CHAR, CMD_DELIMITER } from "../config.ts";
 
 const handleMessages = async (message: WAWebJS.Message) => {
   const { body } = message;
 
-  console.log("Message Recieved:");
   console.log(body);
 
   if (body.startsWith(CMD_CHAR)) {
-    const splitIdx = body.indexOf("@") < 0 ? undefined : body.indexOf("@");
-    const command = body.slice(1, splitIdx);
-    const args = body.slice(splitIdx + 1).split("@");
-
-    console.log({ body, command, args, splitIdx });
+    const strippedCmd = body.slice(1);
+    const [command, args] = strippedCmd.split(CMD_DELIMITER) as [
+      Command,
+      unknown
+    ];
 
     switch (command) {
-      case "ping": {
-        await message.reply("pong");
+      case "help": {
+        await message.reply(`
+          Available commands:\n
+          "/ping"\n
+          "/login".`);
 
         break;
       }
 
-      case "help": {
-        await message.reply(`
-        Available Commands:\n
-        1: !ping.`);
+      case "ping": {
+        await message.reply("pong");
 
         break;
       }
@@ -35,17 +36,11 @@ const handleMessages = async (message: WAWebJS.Message) => {
 
         const phoneNumber = senderId.replace("@c.us", "").replace("@g.us", "");
 
-        await message.reply(`
-        Enter your fullname\n
-        example: MohsenShamsitabar`);
-
         break;
       }
 
       default: {
-        await message.reply(
-          "Unrecognized command. Try !ping, !hello, or !info."
-        );
+        await message.reply(`Unrecognized command. Try "${CMD_CHAR}help".`);
 
         break;
       }
