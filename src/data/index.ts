549134834426e95f80168a78ec9b3c07d6a575cc
Mohsen Shamsitabar <shamsitabar.mohsen@gmail.com>
2025-04-15 13:46:22 +0330

    remove `data`

--- src/data/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as JsonStorage } from "./json-storage.ts";
-export { default as registeredUsers } from "./registered-users/index.ts";
--- src/data/registered-users/registered-users.ts
+++
@@ -1,8 +0,0 @@
-import JsonStorage from "../json-storage.ts";
-
-const registeredUsers = new JsonStorage({
-  filePath: "./registered-users.json",
-  prettyPrint: true
-});
-
-export default registeredUsers;
