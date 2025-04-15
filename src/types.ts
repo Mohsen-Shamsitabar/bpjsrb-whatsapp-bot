57e88e449418b3d0d03d4dffb0170c1c5d1abf41
Mohsen Shamsitabar <shamsitabar.mohsen@gmail.com>
2025-04-15 13:46:46 +0330

    refactor types

--- src/types.ts
+++ src/types.ts
@@ -3,12 +3,13 @@ import type { UserPositions } from "./enums.ts";
 // === === === WWEB === === === //
 
 export type User = {
-  // works like ID.
   phoneNumber: string;
 
   name: string;
   position: UserPositions;
 };
 
-export type UsersMap = Map<string, User>;
-export type UsersRecord = Record<string, User>;
+export type CheckInfo = User & {
+  date: string;
+  time: string;
+};
