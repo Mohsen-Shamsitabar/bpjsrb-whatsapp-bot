4022c49f83b5e73da428bdfa139cd94a22c6f78b
Mohsen Shamsitabar <shamsitabar.mohsen@gmail.com>
2025-04-19 21:34:30 +0330

    refactor storage manager

--- src/storage-manager/initiate-storages.ts
+++ src/storage-manager/initiate-storages.ts
@@ -1,12 +1,16 @@
+import { CACHE_DIR } from "../constants.ts";
+import { CacheIds } from "../enums.ts";
 import checkList from "./storages/checked-list.ts";
 import { registeredUsers } from "./storages/index.ts";
 
 const initiateStorages = () => {
-  registeredUsers.save();
+  registeredUsers.loadFile(`${CACHE_DIR}/${CacheIds.REGISTERED_USERS}`);
   registeredUsers.startAutoPersist();
+  registeredUsers.save();
 
-  checkList.save();
+  checkList.loadFile(`${CACHE_DIR}/${CacheIds.CHECK_LIST}`);
   checkList.startAutoPersist();
+  checkList.save();
 };
 
 export default initiateStorages;
